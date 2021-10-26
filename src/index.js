ee8218285917a160a853554dd3f55a0818e987e7
necmigunduz <necmigunduz@gmail.com>
2021-10-26 10:34:59 +0300

    Add redux dev tools extension

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { thunk } from 'redux-thunk';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS__ && window.__REDUX_DEVTOOLS__()
+);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
